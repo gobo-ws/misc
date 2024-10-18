e0416296763d7c56cdd6bc0916de1d5f34cad18b
Johan Nilsson <jn@gobo.ws>
2024-10-18 17:33:49 +0200

    Now using KVS to store the variables with some minor changes

--- testing/vp/cc.js
+++ testing/vp/cc.js
@@ -1,93 +1,123 @@
+// CueTrigger for Shelly
 // Shelly Plus i4 switch/push button to Visual Productions CueCore 3, CueCore 2, QuadCore & Cuety LPU-2
-// Copyright (C) 2024 Johan Nilsson. https://gobo.ws
+// Copyright © 2024 Johan Nilsson - https://gobo.ws. All rights reserved. 
+// Visual Productions and Shelly are trademarks of their respective owners.
 
-// Configuration
-let CONFIG = {
+// Use the following URL to update a KVS variable on a Shelly device (this example sets cue with ID 1 to 3):
+// http://<hostname/ip>/rpc/KVS.Set?key="cueId1"&value="3"
+
+// After updating a KVS variable on the Shelly you need to restart the script:
+// Stop the script with ID 1:
+// http://<hostname/ip>/rpc/Script.Stop?id=1
+
+// Start the script with ID 1:
+// http://<hostname/ip>/rpc/Script.Start?id=1
+
+
+// Default configuration
+let defaultConfig = {
     hostname: 'haarlem.visualproductions.nl:91', // Device hostname/IP
     mode: 'switch', // Options: 'switch' or 'pushbutton'
     deviceType: 'CueCore', // Options: 'CueCore', 'QuadCore', 'CuetyLPU2'
-
-    // Playback IDs for each switch/push button. CueCore3 (01-16), CueCore2 (01-08), QuadCore (01-06), Cuety LPU-2 (01-64)
+	
+    // Playback IDs for each push button. CueCore3 (01-16), CueCore2 (01-08), QuadCore (01-06), Cuety LPU-2 (01-64)
     pbId1: '01', // Playback ID for switch/push button 1
     pbId2: '01', // Playback ID for switch/push button 2
     pbId3: '01', // Playback ID for switch/push button 3
     pbId4: '01', // Playback ID for switch/push button 4
-
+	
     // Cue IDs for each switch/push button (1-32) except for Cuety LPU-2 (1-48)
     cueId1: 1, // Cue ID for switch/push button 1
     cueId2: 2, // Cue ID for switch/push button 2
     cueId3: 3, // Cue ID for switch/push button 3
-    cueId4: 4, // Cue ID for switch/push button 4
+    cueId4: 4  // Cue ID for switch/push button 4
 };
 
-const API_ENDPOINT = '/ajax'; 
+// Load configuration from KVS or fallback to default
+function loadConfigFromKVS() {
+    CONFIG = Object.assign({}, defaultConfig);
+    let keys = Object.keys(defaultConfig);
+    
+    let loadNextConfig = function(index) {
+        if (index < keys.length) {
+            let key = keys[index];
+            Shelly.call("KVS.Get", { key: key }, function (result, error_code, error_message) {
+                if (error_code === 0 && result.value) {
+                    CONFIG[key] = result.value;
+                } else {
+                    Shelly.call("KVS.Set", { key: key, value: defaultConfig[key] });
+                }
+                loadNextConfig(index + 1);
+            });
+        } else {
+            runCueTrigger();
+        }
+    };
+    
+    loadNextConfig(0);
+}
 
-// Log that the script is running
-print("Shelly to " + CONFIG.deviceType + " (" + CONFIG.hostname + ") cue trigger is running in " + CONFIG.mode + " mode");
+// Save configuration to KVS
+function saveConfigToKVS() {
+    let keys = Object.keys(CONFIG);
+    keys.forEach(function(key) {
+        Shelly.call("KVS.Set", { key: key, value: CONFIG[key] }, function (result, error_code, error_message) {
+            if (error_code === 0) {
+                print("Configuration saved to KVS: " + key);
+            } else {
+                print("Error saving config to KVS: " + key + ", " + error_message);
+            }
+        });
+    });
+}
 
-// Construct the API URL based on device type and action
+// Construct the API URL
 function getApiUrl(pbId, cueId, action) {
-    let url = 'http://' + CONFIG.hostname + API_ENDPOINT + '/pb/' + pbId;
+    let url = 'http://' + CONFIG.hostname + '/ajax/pb/' + pbId;
     if (CONFIG.deviceType === 'CuetyLPU2' || CONFIG.deviceType === 'QuadCore') {
-        if (action === 'jump') {
-            url += '/jmp=' + cueId;
-        } else if (action === 'release') {
-            url += '/rel';
-        }
+        url += action === 'jump' ? '/jmp=' + cueId : '/rel';
     } else {
-        if (action === 'jump') {
-            url += '/jump=' + cueId;
-        } else if (action === 'release') {
-            url += '/release';
-        }
+        url += action === 'jump' ? '/jump=' + cueId : '/release';
     }
     return url;
 }
 
-// Function to send playlist command based on switch state
+// Send commands based on switch state
 function sendPlaylistCommand(switchId, state) {
     let cueId = CONFIG['cueId' + switchId];
     let pbId = CONFIG['pbId' + switchId];
-    
-    let action = state === true ? 'jump' : 'release';
+    let action = state ? 'jump' : 'release';
     let url = getApiUrl(pbId, cueId, action);
-
-    // Log the triggering or stopping action
-    print((state === true ? 'Triggering' : 'Stopping') + ' cue: ' + cueId + ' in playback: ' + pbId);
-
-    Shelly.call("http.request", {
-        method: "GET",
-        url: url
-    });
+    print((state ? 'Triggering' : 'Stopping') + ' cue: ' + cueId + ' in playback: ' + pbId);
+    Shelly.call("http.request", { method: "GET", url: url });
 }
 
-// Function to trigger a cue
+// Trigger a cue in push button mode
 function triggerCue(switchId) {
     let cueId = CONFIG['cueId' + switchId];
     let pbId = CONFIG['pbId' + switchId];
-    
     let url = getApiUrl(pbId, cueId, 'jump');
-    
-    // Log the triggering action
     print('Triggering cue: ' + cueId + ' in playback: ' + pbId);
-
-    Shelly.call("http.request", {
-        method: "GET",
-        url: url
-    });
+    Shelly.call("http.request", { method: "GET", url: url });
 }
 
 // Event handler for input events
 Shelly.addEventHandler(function (event) {
-    if (event && event.name && event.name === 'input') {  
+    if (event.name === 'input') {
         if (CONFIG.mode === 'switch') {
             if (event.info && event.info.state !== undefined) {
                 sendPlaylistCommand(event.id + 1, event.info.state);
             }
-        } else if (CONFIG.mode === 'pushbutton') {
-            if (event.info && event.info.state === true) {
-                triggerCue(event.id + 1);
-            }
+        } else if (CONFIG.mode === 'pushbutton' && event.info.state === true) {
+            triggerCue(event.id + 1);
         }
     }
 });
+
+// Initialize the script
+function runCueTrigger() {
+    print("CueTrigger for Shelly is running in " + CONFIG.mode + " mode. Device hostname/IP: " + CONFIG.hostname + "");
+}
+
+// Loading the configuration
+loadConfigFromKVS();
